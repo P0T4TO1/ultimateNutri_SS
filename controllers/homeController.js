72792b1620e4cf19c570070c19d8be78d5bec1fd
Jaret Garcia <jgarciag2005@alumno.ipn.mx>
2024-03-24 16:04:44 -0600

    first commit

---
+++ controllers/homeController.js
@@ -0,0 +1,9 @@
+module.exports = {
+  index: function (req, res, next) {
+    res.render("home", {
+      title: "Nutri2",
+      isAuthenticated: req.isAuthenticated(),
+      user: req.user,
+    });
+  },
+};
---
+++ database/config.js
@@ -0,0 +1,16 @@
+// var config = {
+//   host: "localhost",
+//   user: "root",
+//   password: "227430Jxgg.",
+//   password: "n0m3l0",
+//   database: "nutribase",
+// };
+
+var config = {
+  host: "mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  user: "vml78s7ow4gwekf2",
+  password: "wdjb5v5aej9tdraz",
+  database: "mqj405zk7pm4cb6l",
+};
+
+module.exports = config;
