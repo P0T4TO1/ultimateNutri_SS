f1fcb480b516ae63b909659210b678c66c826533
P0T4TO1 <jgarciag2005@alumno.ipn.mx>
2024-04-14 00:47:18 -0600

    Nodemon add for dev enviroment

--- app.js
+++ app.js
@@ -37,8 +37,6 @@ app.use(passport.session());
 
 app.use('/', indexRouter);
 
-
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- public/js/konami.js
+++ public/js/konami.js
@@ -1,21 +1,32 @@
 // a key map of allowed keys
 var allowedKeys = {
-  37: 'left',
-  38: 'up',
-  39: 'right',
-  40: 'down',
-  65: 'a',
-  66: 'b'
+  37: "left",
+  38: "up",
+  39: "right",
+  40: "down",
+  65: "a",
+  66: "b",
 };
 
 // the 'official' Konami Code sequence
-var konamiCode = ['up', 'up', 'down', 'down', 'left', 'right', 'left', 'right', 'b', 'a'];
+var konamiCode = [
+  "up",
+  "up",
+  "down",
+  "down",
+  "left",
+  "right",
+  "left",
+  "right",
+  "b",
+  "a",
+];
 
 // a variable to remember the 'position' the user has reached so far.
 var konamiCodePosition = 0;
 
 // add keydown event listener
-document.addEventListener('keydown', function(e) {
+document.addEventListener("keydown", function (e) {
   // get the value of the key code from the key map
   var key = allowedKeys[e.keyCode];
   // get the value of the required key from the konami code
@@ -23,7 +34,6 @@ document.addEventListener('keydown', function(e) {
 
   // compare the key with the required key
   if (key === requiredKey) {
-
     // move to the next key in the konami code sequence
     konamiCodePosition++;
 
@@ -37,13 +47,11 @@ document.addEventListener('keydown', function(e) {
   }
 });
 
-function activateCheats()  {
-
-    var x = document.getElementById("nf");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-        
-    }
+function activateCheats() {
+  var x = document.getElementById("nf");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
 }
